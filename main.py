64ef0a28bbe7b3d544cadb9e328bbfcb8c0cff76
𝑫𝑽𝑴𝑩 <infentronthegamer@gmail.com>
2025-10-19 13:44:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,48 @@
 import requests
 import sys
-import time
+
 
 api = "https://duvm-mem-api-checker.vercel.app/"
 
 ch = "@unsely"
 
-RED = "\033[31m";GREEN = "\033[32m";CYAN = "\033[36m";RESET = "\033[0m"
+red = "\033[31m";green = "\033[32m";cya = "\033[36m";reset = "\033[0m"
 
 def userid():
     while True:
         try:
-            return int(input(f"{GREEN}Enter your Telegram User ID: {CYAN}"))
+            
+            anim(f""" ㅤ{white}ㅤ[ ⚚ ]    𝐄𝚗𝚝𝚎𝚛 𝐈𝙳 𝐁𝚎𝚕𝚘𝚠 𝐓𝚘 𝐂𝚑𝚎𝚌𝚔 ⏎{reset}""")
+            return int(input(f" ㅤ{niggerz()}ㅤ➡  ㅤ"))
         except ValueError:
-            print(f"{RED}Invalid input. Please enter a numeric Telegram User ID.{RESET}")
+            anim(f""" ㅤ{red}ㅤ[ ⚚ ]    𝐈𝚜 𝐘𝚘𝚞𝚛 𝐁𝚛𝚊𝚒𝚗 𝐋𝚘𝚌𝚊𝚝𝚎𝚍 𝐈𝚗 𝐘𝚘𝚞𝚛 𝐀𝚜𝚜?  {reset}""")
 
-def mem(userid):
+def mem(uid):
     try:
-        response = requests.get(api, params={"user_id": userid}, timeout=10)
+        response = requests.get(api, params={"user_id": uid}, timeout=10)
         response.raise_for_status()
         data = response.json()
     except requests.RequestException as e:
-        print(f"{RED}[✖] Failed to reach API: {e}{RESET}")
+        anim(f" ㅤ{red}ㅤ[ ⚚ ]    𝐔𝚗𝚊𝚋𝚕𝚎 𝐓𝚘 𝐅𝚎𝚝𝚌𝚑 𝐀𝚙𝚒 {e}")
         sys.exit(1)
-    except ValueError:
-        print(f"{RED}[✖] Invalid JSON response from API.{RESET}")
+    except ValueError as e:
+        anim(f" ㅤ{red}ㅤ[ ⚚ ]    𝐈𝚗𝚟𝚊𝚕𝚒𝚍 𝐑𝚎𝚜𝚙𝚘𝚗𝚜𝚎 𝐅𝚛𝚘𝚖 𝐀𝚙𝚒 {e}")
         sys.exit(1)
 
     if data.get("ok") and data.get("member"):
-        print(f"{GREEN}[✔] Access Granted: User is a member ({data.get('status')}){RESET}")
+        print("")
     else:
 
-        print(f"{RED}[✖] Access Denied: Please join required channel {ch} so you can use this tool.{RESET}")
+        anim(f" ㅤ{red}ㅤ[ ⚚ ]    𝐉𝚘𝚒𝚗 𝐀𝚕𝚕 𝐂𝚑𝚊𝚗𝚗𝚎𝚕𝚜 / 𝐀𝚍𝚍 𝐅𝚘𝚕𝚍𝚎𝚛")
         sys.exit(0)
 
-print(f"{CYAN}Telegram Channel Access Verification via Vercel API{RESET}")
+anim(f" ㅤ{white}ㅤ[ ⚚ ]    𝐓𝚘𝚘𝚕 𝐖𝚒𝚕𝚕 𝐎𝚗𝚕𝚢 𝐖𝚘𝚛𝚔 𝐈𝚏 𝐘𝚘𝚞 𝐇𝚊𝚟𝚎 𝐉𝚘𝚒𝚗𝚎𝚍 𝐀𝚕𝚕 𝐓𝚑𝚎 𝐂𝚑𝚊𝚗𝚗𝚎𝚕𝚜")
 uid = userid()
-print(f"{CYAN}Verifying access for User ID: {uid}...{RESET}")
-mem(userid)
 
-print(f"{GREEN}Membership verified successfully. You may now use this tool.{RESET}")
+mem(userid)
+anim(f" ㅤ{white}ㅤ[ ⚚ ]    𝐆𝚘𝚘𝚍 𝐁𝚘𝚢")
 
 print(f"{GREEN}Starting main tool...{RESET}")
+
+
+shit = input("working bitch")
